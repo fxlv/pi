bf05eab3638b7feb8a2754b73b678f5fe6ff71f4
Kaspars Mickevics <kaspars@fx.lv>
2015-03-03 20:25:18 +0200

    Added external.py

---
+++ temperature/external.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# get temperature data from external sources
+# currently that means only from foreacast.io API 
+# using the forecastio python library
+
+import forecastio
+import account
+from azurepy import queues
+
+# Tallinn, Kesklinn
+lat, lng = 59.41494, 24.74032
+# my forecast.io key, kept in a separate file
+key = account.forecast_io_key
+
+forecast = forecastio.load_forecast(key, lat, lng)
+current_data = forecast.currently()
+forecastio_temperature = current_data.d['temperature']
+forecastio_queue = queues.Queue("forecastio-temperature")
+forecastio_queue.put(forecastio_temperature)
