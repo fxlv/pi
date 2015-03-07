2db301f3bcbab7c8b61f0456c2a577dd017ea7b0
Kaspars Mickevics <kaspars@fx.lv>
2015-03-07 19:29:18 +0000

    Temperature should be a FLOAT in the database

--- temperature/storage/create.sql
+++ temperature/storage/create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "temperature" (
     `id`    INTEGER NOT NULL,
     `source_name`   VARCHAR(200),
-    `temperature`   VARCHAR(200),
+    `temperature`   FLOAT,
     `insert_time`   DATE,
     PRIMARY KEY(id)
 );
