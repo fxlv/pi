2cdc8083bf68fc605f8a291623258a2f2c991a55
Kaspars Mickevics <kaspars@fx.lv>
2015-03-12 08:52:26 +0200

    Added missing tests

--- temperature/storage/tempdb_test.py
+++ temperature/storage/tempdb_test.py
@@ -19,9 +19,6 @@ def test_connect():
     assert isinstance(conn, sqlite3.Connection)
     assert isinstance(cursor, sqlite3.Cursor)
 
-def test_cleanup():
-    os.unlink(test_db_name)
-
 def test_add_reading():
     test_temperature = random.randint(-10,30)
     t.add_reading(test_source_name, test_temperature)
@@ -29,10 +26,22 @@ def test_add_reading():
     t.add_reading(test_source_name, test_temperature)
 
 def test_get_source_names():
-    pass
+    source_names = t.get_source_names()
+    assert len(source_names) == 1
+    assert source_names[0] == test_source_name
 
 def test_get_all_readings():
-    pass
+    readings = t.get_all_readings(test_source_name)
+    assert len(readings) == 2
+    # second column of the first row contains source name
+    assert readings[0][1] == test_source_name
 
 def test_get_last_reading():
-    pass
+    last_reading = t.get_last_reading(test_source_name)
+    # should contain 5 columns/fields
+    assert len(last_reading) == 5
+    assert isinstance(last_reading, tuple)
+    assert last_reading[1] == test_source_name
+
+def test_cleanup():
+    os.unlink(test_db_name)
