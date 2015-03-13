30948bb9327e8ffb6c4fd3f8f5b40e2845b643dc
Kaspars Mickevics <kaspars@fx.lv>
2015-03-13 19:33:46 +0200

    Return false instead of number when failing.

--- temperature/ilmerree.py
+++ temperature/ilmerree.py
@@ -1,23 +1,20 @@
 """
-Get temperature from http://ilm.err.ee/ 
+Get temperature from http://ilm.err.ee/
 """
 from bs4 import BeautifulSoup
 import requests
+
 
 def get_html():
     page = requests.get("http://ilm.err.ee")
     return page.text
 
+
 def get_temperature():
     soup = BeautifulSoup(get_html(), "lxml")
     try:
-        temperature = soup.find("div","ilm_tana_parem").string.strip()
+        temperature = soup.find("div", "ilm_tana_parem").string.strip()
         temperature = float(temperature.split()[0])
     except AttributeError:
-        temperature = 777 
-    except ValueError:
-        temperature = 888
-        
-    # split off the celsius symbol and make it a float instead of string
+        return False
     return temperature
-
