7d04abfdb2300a3a7add9a30d14ff3c3d3cf6bd4
Kaspars Mickevics <kaspars@fx.lv>
2015-03-18 06:47:17 +0000

    Use TIMESTAMP instead of DATE for DB

--- temperature/storage/create.sql
+++ temperature/storage/create.sql
@@ -2,7 +2,7 @@ CREATE TABLE "temperature" (
     `id`    INTEGER NOT NULL,
     `source_name`   VARCHAR(200),
     `temperature`   FLOAT,
-    `insert_time`   DATE,
+    `insert_time`   TIMESTAMP,
     `success`   BOOLEAN,
     PRIMARY KEY(id)
 );
