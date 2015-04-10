31648b03cb0baf961439b2e3679489d4b270dc20
Kaspars Mickevics <kaspars@fx.lv>
2015-04-10 05:55:14 +0000

    Better retry mechanism for backups

--- temperature/backup_db.sh
+++ temperature/backup_db.sh
@@ -4,9 +4,19 @@
 #    * * * * * cd ${HOME}/pi/temperature; ./backup_db.sh
 # to your crontab
 #
-if ! sqlite3 temperature.db ".backup temperature.backup.db"; then
-    sleep 5;
-    sqlite3 temperature.db ".backup temperature.backup.db";
-fi
+function backup {
+    sqlite3 temperature.db ".backup temperature.backup.db" 2>/dev/null;
+}
 
-cp temperature.backup.db /home/temperature/temperature.db
+# the database can be locked when new data is being imported
+# so we use simple retry mechanism
+while true; do
+    if backup; then
+        # if backup was successful, copy the backup file
+        # to a place where it will be accessible by remote ssh
+        cp temperature.backup.db /home/temperature/temperature.db
+        exit 0
+    else
+        sleep 5
+    fi
+done
