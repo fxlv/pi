a8165ba40c5ac1b1a09611c6a16f58b25db9e3f3
Kaspars Mickevics <kaspars@fx.lv>
2015-06-05 15:44:47 +0300

    Tempy.py updated

--- temperature/tempy.py
+++ temperature/tempy.py
@@ -1,4 +1,30 @@
 import requests
-def update(data):
-    requests.post("http://tempy.tln/add",data=data)
 
+tempy_url = "http://tempy.tln"
+
+
+def get_last(sensor_name):
+    url = "{0}/get/{1}/last".format(tempy_url, sensor_name)
+    print "url:", url
+    result = requests.get(url).json
+    print "Debug:",result
+    return result
+
+
+def get_temperature(sensor_name):
+    temperature_data = get_last(sensor_name)
+    if "temperature" in temperature_data:
+        return temperature_data["temperature"]
+    return False
+
+
+def get_humidity(sensor_name):
+    temperature_data = get_last(sensor_name)
+    if "humidity" in temperature_data:
+        return temperature_data["humidity"]
+    return False
+
+
+def update(data):
+    url = "{}/add".format(tempy_url)
+    requests.post(url, data=data)
